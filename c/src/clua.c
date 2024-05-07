af7929cfff7353a3865eeaf8d8a058f28be2cb6c
flucium <flucium@flucium.net>
2024-05-07 13:07:11 +0900

    add ./c/src/clua.c

---
+++ c/src/clua.c
@@ -0,0 +1,29 @@
+#include "../../lua-5.4.6/src/lua.h"
+#include "../../lua-5.4.6/src/lauxlib.h"
+#include "../../lua-5.4.6/src/lualib.h"
+
+extern int eval(const char *code);
+
+int eval(const char *code)
+{
+    lua_State *L = luaL_newstate();
+    
+    luaL_openlibs(L);
+
+    if (luaL_dostring(L, code))
+    {
+        const char *error = lua_tostring(L, -1);
+    
+        printf("Error: %s\n", error);
+    
+        lua_pop(L, 1);
+    
+        lua_close(L);
+    
+        return 1;
+    }
+
+    lua_close(L);
+    
+    return 0;
+}
