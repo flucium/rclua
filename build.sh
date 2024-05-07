4367bfc892fb2648fe4099780e421753fbf16cb6
flucium <flucium@flucium.net>
2024-05-07 14:44:50 +0900

    update ./build.sh

--- build.sh
+++ build.sh
@@ -40,6 +40,19 @@ test()
   cargo test
 }
 
+package(){
+  cd lua-5.4.6/src
+  make
+  ar rcs liblua.a ./*.o
+  mv ./liblua.a ../liblua.a
+  cd ../../c/src
+  gcc -c ./clua.c -o clua.o
+  ar rcs libclua.a clua.o
+  mv ./libclua.a ../libclua.a
+  cd ../..
+  cargo package --allow-dirty
+}
+
 clean()
 {
   cargo clean
