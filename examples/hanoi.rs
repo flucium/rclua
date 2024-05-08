052974a6f4651cec3b3ced69db0fd99f62bff787
flucium <flucium@flucium.net>
2024-05-08 15:31:48 +0900

    rm -r ./examples
    this is the operation involved in adding a Lua version. example code removed.

--- examples/hanoi.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    std::fs::read_to_string("./examples/hanoi.lua")
-        .map(|script| rclua::eval(&script, rclua::Version::default()))
-        .unwrap_or_else(|e| {
-            eprintln!("Error reading file: {}", e);
-        });
-}
--- examples/hello_world.rs
+++
@@ -1,5 +0,0 @@
-use rclua::{eval, Version};
-
-fn main() {
-    eval("print('Hello, World!')\0", Version::default());
-}
