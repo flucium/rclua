73294bd4f84c2349e44e2faa198e0bdf9a9d556a
flucium <flucium@flucium.net>
2024-05-08 15:25:11 +0900

    update ./build.sh

--- build.sh
+++ build.sh
@@ -1,63 +1,105 @@
-release()
+lua_5_4_6()
 {
+  # Lua 5.4.6
   cd lua-5.4.6/src
   make
   ar rcs liblua.a ./*.o
   mv ./liblua.a ../liblua.a
-  cd ../../c/src
-  gcc -c ./clua.c -o clua.o
-  ar rcs libclua.a clua.o
-  mv ./libclua.a ../libclua.a
+
+  # Back to root
   cd ../..
+}
+
+lua_5_4_0()
+{
+  # Lua 5.4.0
+  cd lua-5.4.0/src
+  make
+  ar rcs liblua.a ./*.o
+  mv ./liblua.a ../liblua.a
+
+  # Back to root
+  cd ../..
+}
+
+clua_5_4_6()
+{
+  # To CLua
+  cd c/src
+  
+  # CLua 5.4.6
+  gcc -c ./clua_5_4_6.c -o clua_5_4_6.o
+  ar rcs libclua_5_4_6.a clua_5_4_6.o
+  mv ./libclua_5_4_6.a ../libclua_5_4_6.a
+
+  # Back to root
+  cd ../..
+}
+
+clua_5_4_0()
+{
+  # To CLua
+  cd c/src
+
+  # CLua 5.4.0
+  gcc -c ./clua_5_4_0.c -o clua_5_4_0.o
+  ar rcs libclua_5_4_0.a clua_5_4_0.o
+  mv ./libclua_5_4_0.a ../libclua_5_4_0.a
+
+  # Back to root
+  cd ../..
+}
+
+doc()
+{
+  cargo doc
+}
+
+doc_open()
+{
+  cargo doc --open
+}
+
+release()
+{
+  lua_5_4_6
+  lua_5_4_0
+  clua_5_4_6
+  clua_5_4_0
   cargo build --release
 }
 
 debug()
 {
-  cd lua-5.4.6/src
-  make
-  ar rcs liblua.a ./*.o
-  mv ./liblua.a ../liblua.a
-  cd ../../c/src
-  gcc -c ./clua.c -o clua.o
-  ar rcs libclua.a clua.o
-  mv ./libclua.a ../libclua.a
-  cd ../..
+  lua_5_4_6
+  lua_5_4_0
+  clua_5_4_6
+  clua_5_4_0
   cargo build
 }
 
-test()
-{
-  cd lua-5.4.6/src
-  make
-  ar rcs liblua.a ./*.o
-  mv ./liblua.a ../liblua.a
-  cd ../../c/src
-  gcc -c ./clua.c -o clua.o
-  ar rcs libclua.a clua.o
-  mv ./libclua.a ../libclua.a
-  cd ../..
-  cargo test
-}
-
-package(){
-  cd lua-5.4.6/src
-  make
-  ar rcs liblua.a ./*.o
-  mv ./liblua.a ../liblua.a
-  cd ../../c/src
-  gcc -c ./clua.c -o clua.o
-  ar rcs libclua.a clua.o
-  mv ./libclua.a ../libclua.a
-  cd ../..
+package()
+{ 
+  debug
+  release
   cargo package --allow-dirty
 }
 
 clean()
 {
+  # Remove ./lua-*/src/*.o
+  rm ./lua-*/src/*.o
+  
+  # Remove ./lua-*/*.a
+  rm ./lua-*/*.a
+
+  # Remove ./c/src/*.o
+  rm ./c/src/*.o
+
+  # Remove ./c/*.a
+  rm ./c/*.a
+
   cargo clean
-  rm ./c/*.a ./c/src/*.o
-  rm ./lua-5.4.6/src/*.o ./lua-5.4.6/liblua.a
 }
 
 for entry in "$@" ; do
