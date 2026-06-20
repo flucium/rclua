e91e563450bde472f7a3f87b5a80cd5a8ca1eb51
flucium <flucium@flucium.net>
2026-06-20 20:43:31 +0900

    update ./benches/bench.rs
    added Lua 5.5.0. bench.

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,7 +3,7 @@
 extern crate test;
 
 #[bench]
-fn bench_lua_5_4_0(b: &mut test::Bencher) {
+fn bench_lua_5_5_0(b: &mut test::Bencher) {
     const CODE:&str = r"
 local count = 0
 
@@ -21,10 +21,9 @@ hanoi(5, 'a', 'b', 'c')
 ";
 
     b.iter(|| {
-        rclua::execute(CODE, rclua::Version::V5_4_0);
+        rclua::execute(CODE, rclua::Version::V5_5_0);
     });
 }
-
 
 #[bench]
 fn bench_lua_5_4_6(b: &mut test::Bencher) {
@@ -48,3 +47,28 @@ hanoi(5, 'a', 'b', 'c')
         rclua::execute(CODE, rclua::Version::V5_4_6);
     });
 }
+
+#[bench]
+fn bench_lua_5_4_0(b: &mut test::Bencher) {
+    const CODE:&str = r"
+local count = 0
+
+function hanoi(n, a, b, c)
+    if n > 0 then
+        hanoi(n-1, a, c, b)
+        
+        count = count + 1
+        
+        hanoi(n-1, c, b, a)
+    end
+end
+
+hanoi(5, 'a', 'b', 'c')
+";
+
+    b.iter(|| {
+        rclua::execute(CODE, rclua::Version::V5_4_0);
+    });
+}
+
+
