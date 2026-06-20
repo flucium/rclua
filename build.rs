e2c42d344e74b03413000c598054d7c7fd855b56
flucium <flucium@flucium.net>
2026-06-20 20:41:40 +0900

    update ./build.rs
    added Link Lua 5.5.0.

--- build.rs
+++ build.rs
@@ -1,15 +1,19 @@
 const CARGO_RUSTC_LINK_SEARCH_NATIVE_C: &str = "cargo:rustc-link-search=native=./c";
 const CARGO_RUSTC_LINK_LIB_STATIC_LUA: &str = "cargo:rustc-link-lib=static=lua";
+const CARGO_RUSTC_LINK_SEARCH_NATIVE_LUA_5_5_0: &str = "cargo:rustc-link-search=native=./lua-5.5.0";
 const CARGO_RUSTC_LINK_SEARCH_NATIVE_LUA_5_4_6: &str = "cargo:rustc-link-search=native=./lua-5.4.6";
 const CARGO_RUSTC_LINK_SEARCH_NATIVE_LUA_5_4_0: &str = "cargo:rustc-link-search=native=./lua-5.4.0";
+const CARGO_RUSTC_LINK_LIB_STATIC_CLUA_5_5_0: &str = "cargo:rustc-link-lib=static=clua_5_5_0";
 const CARGO_RUSTC_LINK_LIB_STATIC_CLUA_5_4_6: &str = "cargo:rustc-link-lib=static=clua_5_4_6";
 const CARGO_RUSTC_LINK_LIB_STATIC_CLUA_5_4_0: &str = "cargo:rustc-link-lib=static=clua_5_4_0";
 
 fn main() {
     println!("{}", CARGO_RUSTC_LINK_SEARCH_NATIVE_C);
     println!("{}", CARGO_RUSTC_LINK_LIB_STATIC_LUA);
+    println!("{}", CARGO_RUSTC_LINK_SEARCH_NATIVE_LUA_5_5_0);
     println!("{}", CARGO_RUSTC_LINK_SEARCH_NATIVE_LUA_5_4_6);
     println!("{}", CARGO_RUSTC_LINK_SEARCH_NATIVE_LUA_5_4_0);
+    println!("{}", CARGO_RUSTC_LINK_LIB_STATIC_CLUA_5_5_0);
     println!("{}", CARGO_RUSTC_LINK_LIB_STATIC_CLUA_5_4_6);
     println!("{}", CARGO_RUSTC_LINK_LIB_STATIC_CLUA_5_4_0);
 }
