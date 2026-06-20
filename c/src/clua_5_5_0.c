c7714390202dbba09aefad04e4637eb2ecfb7053
flucium <flucium@flucium.net>
2026-06-20 20:40:09 +0900

    add lua 5.5.0, execute. c source code.

---
+++ c/src/clua_5_5_0.c
@@ -0,0 +1,29 @@
+#include "../../lua-5.5.0/src/lua.h"
+#include "../../lua-5.5.0/src/lauxlib.h"
+#include "../../lua-5.5.0/src/lualib.h"
+
+extern int execute_5_5_0(const char *code);
+
+int execute_5_5_0(const char *code)
+{
+    lua_State *L = luaL_newstate();
+    
+    luaL_openlibs(L);
+
+    if (luaL_dostring(L, code))
+    {
+        const char *error = lua_tostring(L, -1);
+    
+        printf("Error: %s\n", error);
+    
+        lua_pop(L, 1);
+    
+        lua_close(L);
+    
+        return 1;
+    }
+
+    lua_close(L);
+    
+    return 0;
+}
